b7bb7abeaa0330563f8cf6dc15abaff97fb9f119
Mohammad <msalam6@my.centennialcollege.com>
2024-04-06 17:38:16 -0400

    working...

--- client/src/nurse/CreateVisits.jsx
+++ client/src/nurse/CreateVisits.jsx
@@ -1,2 +1,8 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { gql } from 'apollo-boost';
+
 //This will component create and record visits
 //i may integrate this component with Create Vitals later
+
+
