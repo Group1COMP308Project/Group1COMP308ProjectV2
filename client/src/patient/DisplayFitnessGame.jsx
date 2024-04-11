063ae36b43e2d531055ac4734560afe01d557d15
Mohammad <mdsalam1994@hotmail.com>
2024-04-11 02:49:55 -0400

    working...

--- client/src/patient/DisplayFitnessGame.jsx
+++ client/src/patient/DisplayFitnessGame.jsx
@@ -16,23 +16,47 @@ const GET_ACTIVITIES = gql`
 `;
 
 const getActivity = () => {
+    const { loading, error, data, refetch } = useQuery(GET_ACTIVITIES);
+    const fetchGames = ()=> {
+        refetch();
+    }
+
+
 
     if (loading) return <p>Loading...</p>;
 
     return (
 
         <div className="get-activities">
-            <h1>Activities</h1>
-           
+            <h1>List of Activities</h1>
 
+            <button onClick={fetchGames}>Refetch activities</button>  
+            {games.map((activity)=>(
+
+                <div key={activity.id} className="activity">
+                    <div>Id</div>
+                    <div>{activity.id}</div>
+                    <div>Activity</div>
+                    <div>{activity.firstname}
+                    </div>
+                    
+          )}
+
+        
+
+   
         </div>
 
 
-    )
+
+
+    ),
 
 
 
 
 
 
-}
+};
+
+export default getActivity;
